ce4f6250433606da12bc9f3e6d1c0026a7df5059
Nareshgjgembrill <65347949+Nareshgjgembrill@users.noreply.github.com>
2021-08-17 09:48:18 -0400

    check

--- 17thAug/src/pages/index.js
+++ 17thAug/src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your change in view</p>
+    <p>RAVIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
